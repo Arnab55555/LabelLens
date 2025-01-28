0508cb5cc99681b187be73ff557d95dcaefec025
Iliyas-shah <shahiliyas289@gmail.com>
2025-01-28 22:00:45 +0530

    Removed navigation Imports

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,12 @@
 import { ScrollView, StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Homepage from './templates/Homepage'
 
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
 
 const App = () => {
   return (

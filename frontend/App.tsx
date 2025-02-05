748ba3ead6a99e9a8c8d565f392d6bf62b74649d
Iliyas-shah <shahiliyas289@gmail.com>
2025-02-05 16:39:42 +0530

    installed React Navigation and created Navigation Routes for Home,BarcodeScan,ProductSearch,AddProduct

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,83 +1,39 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  TouchableOpacity,
-} from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home';
+import BarcodeScan from './screens/BarcodeScan';
+import ProductSearch from './screens/ProductSearch';
+import AddProduct from './screens/AddProduct';
+
+export type RootStackParamList = {
+    Home: undefined;
+    BarcodeScan: undefined;
+    ProductSearch:undefined;
+    AddProduct:undefined;
+  };
+const Stack = createNativeStackNavigator<RootStackParamList>()
 
 function App(): React.JSX.Element {
 
   return (
-    <SafeAreaView style = {styles.container}>
-        <Text style = {styles.headerText}>📋🔍 LabelLens </Text>
-
-        <TouchableOpacity style = {styles.card}>
-            <Text style = {styles.cardText}>🔍   Search for a product</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity style = {styles.card}>
-            <Text style = {styles.cardText}>📋   Add Product Details</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity style = {styles.photoButton}>
-            <Text style = {styles.photoButtonText}> 📷   Take a Photo of the Label </Text>
-        </TouchableOpacity>
-    </SafeAreaView>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home' 
+        screenOptions={{
+            headerStyle:{
+                backgroundColor:"#4CAF50",
+            },
+        }}
+        >
+            <Stack.Screen name="Home" component={Home}/>
+            <Stack.Screen name="BarcodeScan" component={BarcodeScan}/>
+            <Stack.Screen name="ProductSearch" component={ProductSearch}/>
+            <Stack.Screen name="AddProduct" component={AddProduct}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#FFFFFF',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-},
-headerText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 30,
-    color: '#4CAF50',
-},
-card: {
-    width: '100%',
-    backgroundColor: '#F8F8F8',
-    borderRadius: 10,
-    elevation: 3,
-    padding: 15,
-    marginVertical: 10,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-},
-cardText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#000000',
-},
-photoButton: {
-    width: '100%',
-    backgroundColor: '#4CAF50',
-    borderRadius: 10,
-    padding: 15,
-    marginTop: 100,
-    alignItems: 'center',
-},
-photoButtonText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#FFFFFF',
-},
-
-});
 
 export default App;
---
+++ frontend/screens/ProductSearch.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ProductSearch = () => {
+  return (
+    <View>
+      <Text>ProductSearch</Text>
+    </View>
+  )
+}
+
+export default ProductSearch
